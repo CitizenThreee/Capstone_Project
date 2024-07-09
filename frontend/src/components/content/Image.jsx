647eb81161cb193875ef14c5ab50a5ced686ce4e
CitizenThreee <albeisly@gmail.com>
2024-07-10 00:03:00 +1200

    removed frontend files form backend git

--- frontend/src/components/content/Image.jsx
+++
@@ -1,7 +0,0 @@
-export default function Image({data}) {
-    return(
-        <>
-            <img src={data.url} style={{ width: "45rem", maxWidth: "95vw" }}></img>
-        </>
-    )
-}
--- frontend/src/components/content/Message.jsx
+++
@@ -1,9 +0,0 @@
-export default function Message({data}) {
-    return(
-        <>
-            <div className="p-1 px-2 border mb-3 rounded-2" style={{ width: "45rem", maxWidth: "95vw" }}>
-                {data.message}
-            </div>
-        </>
-    )
-}
