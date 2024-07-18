d14eae6b8eb6eb924a890fc1049764eb2d44c32b
CitizenThreee <albeisly@gmail.com>
2024-07-18 19:15:25 +1200

    Validation testing

--- tests/EndpointTests.js
+++ tests/EndpointTests.js
@@ -1,4 +1,17 @@
-import axios from 'axios'
+const axios = require('axios')
+const data = require('./MockData.json')
 
-console.log('Starting endpoint testing...');
+Test();
 
+async function Test() {
+    console.log('Starting endpoint testing...');
+
+    console.log('Testing user endpoints');
+    console.log('Testing user creation');
+    for (let i = 0; i < data.users.length; i++) {
+        console.log('Testing data: ' + data.users[i]);
+        axios.post('http://localhost:8080/users', data.users[i])
+            .then(res => { if (!res) console.log('No data returned when creating user'); console.log(res) })
+            .catch(err => { console.log('Error when creating user') })
+    }
+}
